b4292a48d7a1a6a129d5728be4d1f2edc1e1cc71
feiiit <lagzinsta@gmail.com>
2024-07-16 23:49:29 +0300

    grazinu default score system

--- js/score.js
+++ js/score.js
@@ -10,25 +10,33 @@ const scale = 3;
  * @param {Number} minPercent Minimum percentage required
  * @returns {Number}
  */
-export function score(rank) {
-    var log13 = Math.log(3 * rank + 10) / Math.log(13);
-    var log5 = Math.log(rank) / Math.log(5);
-
-    if (rank => 1) {
-      if (rank < 11) {
-        return round(250 / log13, 2);
-      } else if (rank > 10) {
-        if (rank < 51) {
-          return round((250 * Math.sin(90 - (1000 / 28745 * rank)) / log5) - 10, 2);
-        }
-        else {
-          return 0;
-        }
-      }
+export function score(rank, percent, minPercent) {
+    if (rank > 150) {
+        return 0;
     }
-  }
+    if (rank > 75 && percent < 100) {
+        return 0;
+    }
 
-export function round(num, scale) {
+    // Old formula
+    /*
+    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    */
+    // New formula
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+
+    score = Math.max(0, score);
+
+    if (percent != 100) {
+        return round(score - score / 3);
+    }
+
+    return Math.max(round(score), 0);
+}
+
+export function round(num) {
     if (!('' + num).includes('e')) {
         return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
     } else {
@@ -37,6 +45,10 @@ export function round(num, scale) {
         if (+arr[1] + scale > 0) {
             sig = '+';
         }
-        return +(Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) + 'e-' + scale);
+        return +(
+            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
+            'e-' +
+            scale
+        );
     }
 }

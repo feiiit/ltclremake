8bcd85432990d436218cf07ba21faf85f354ce8d
Kandy <57315055+KandyMun@users.noreply.github.com>
2024-07-17 01:17:23 +0300

    fixed another scoring bug

--- js/score.js
+++ js/score.js
@@ -30,7 +30,7 @@ const scale = 2;
 export function score(rank){
   var top10Score = 250/(Math.log(3 * rank + 10) / Math.log(13));
   var top11_50Score = (Math.sin(0.08 * (rank + 8.905))*70) + 100;
-  var top51_75Score = 1.83 + (1/(0.0036*(rank-40)))
+  var top51_75Score = 1.83 + (1/(0.0036*(rank-41)))
 
   if(rank => 1){ //rank has to be above 1
     if(rank < 11)//if rank is 10 or above

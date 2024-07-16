8f900259340765511810c7acd39893791af823b0
Kandy <57315055+KandyMun@users.noreply.github.com>
2024-07-16 23:55:05 +0300

    localize changes in util.js

--- js/util.js
+++ js/util.js
@@ -10,7 +10,7 @@ export function embed(video) {
 }
 
 export function localize(num) {
-    return num.toLocaleString(undefined, { minimumFractionDigits: 3 });
+    return toLocaleString(num, { minimumFractionDigits: 3 });
 }
 
 export function getThumbnailFromId(id) {

031213be71af7ba73ef2359690cea17c9e496bcf
slickomode <geometrydashslick@gmail.com>
2024-07-16 20:37:28 +0300

    išversta

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -16,35 +16,35 @@ export default {
     template: `
         <div class="level-authors">
             <template v-if="selfVerified">
-                <div class="type-title-sm">Creator & Verifier</div>
+                <div class="type-title-sm">Sukūrė & verifyino</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
             </template>
             <template v-else-if="creators.length === 0">
-                <div class="type-title-sm">Creator</div>
+                <div class="type-title-sm">Kūrėjas</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
-                <div class="type-title-sm">Verifier</div>
+                <div class="type-title-sm">Verifyino</div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
             </template>
             <template v-else>
-                <div class="type-title-sm">Creators</div>
+                <div class="type-title-sm">Kūrėjai</div>
                 <p class="type-body">
                     <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
                         <span >{{ creator }}</span
                         ><span v-if="index < creators.length - 1">, </span>
                     </template>
                 </p>
-                <div class="type-title-sm">Verifier</div>
+                <div class="type-title-sm">Verifyino</div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
             </template>
-            <div class="type-title-sm">Publisher</div>
+            <div class="type-title-sm">Įkėlė</div>
             <p class="type-body">
                 <span>{{ author }}</span>
             </p>

26daa3e2346ea2dfa5d45aa3fa85b85e4302fd13
feiiit <lagzinsta@gmail.com>
2024-07-16 22:21:24 +0300

    bandome toliau taisyti points

--- js/score.js
+++ js/score.js
@@ -27,20 +27,15 @@ export function score(rank) { //pointu formule
       }
     }
   }
-
-export function round(num) {
-    if (!('' + num).includes('e')) {
-        return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
+  function roundNumber(num, scale) {     //apvalinimas
+    if (!("" + num).includes("e")) {
+      return +(Math.round(num + "e+" + scale) + "e-" + scale);
     } else {
-        var arr = ('' + num).split('e');
-        var sig = '';
-        if (+arr[1] + scale > 0) {
-            sig = '+';
-        }
-        return +(
-            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
-            'e-' +
-            scale
-        );
+      var arr = ("" + num).split("e");
+      var sig = ""
+      if (+arr[1] + scale > 0) {
+        sig = "+";
+      }
+      return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) + "e-" + scale);
     }
-}
+  }

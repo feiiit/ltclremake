35853c7eb6c8e8344534833fbbbd4ff2e598f062
feiiit <lagzinsta@gmail.com>
2024-07-16 22:19:29 +0300

    bandau pridet sena score system

--- js/score.js
+++ js/score.js
@@ -10,31 +10,23 @@ const scale = 3;
  * @param {Number} minPercent Minimum percentage required
  * @returns {Number}
  */
-export function score(rank, percent, minPercent) {
-    if (rank > 50) {
-        return 0;
+export function score(rank) { //pointu formule
+    log13 = Math.log(3 * rank + 10) / Math.log(13);
+    log5 = Math.log(rank) / Math.log(5);
+    if (rank => 1) {
+      if (rank < 11) {
+        return roundNumber(250 / log13, 2);
+      }
+      else if (rank > 10) {
+        if (rank < 51) {
+          return roundNumber((250 * Math.sin(90 - (1000 / 28745 * rank)) / log5) - 10, 2);
+        }
+        else {
+          return 0;
+        }
+      }
     }
-    if (rank > 75 && percent < 100) {
-        return 0;
-    }
-
-    // Old formula
-    /*
-    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-    */
-    // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-
-    score = Math.max(0, score);
-
-    if (percent != 100) {
-        return round(score - score / 3);
-    }
-
-    return Math.max(round(score), 0);
-}
+  }
 
 export function round(num) {
     if (!('' + num).includes('e')) {

a37da8df0656da0b06a67540a49db083930fba4b
slickomode <geometrydashslick@gmail.com>
2024-07-17 13:39:33 +0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -41,7 +41,7 @@ export default {
                     <h1>{{ level.name }}</h1>
                     <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
                     <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
-                    <button @click="location.href="level.nong">
+                    <button @click="location.href="level.nong"> </button>
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">Taškų vertė</div>

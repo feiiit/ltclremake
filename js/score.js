bfb948cb1ce6275c58aada3737c44cf595de84ac
Kandy <57315055+KandyMun@users.noreply.github.com>
2024-07-17 20:52:16 +0300

    Rounding outputting NaN, so fixing that.

--- js/score.js
+++ js/score.js
@@ -23,6 +23,9 @@
 //       }
 //     }
 //   }
+
+const scale = 2; //Select how many decimal digits to round to
+
 export function score(rank){
   var top10Score = 250/(Math.log(3 * rank + 10) / Math.log(13));
   var top11_50Score = (Math.sin(0.08 * (rank + 8.905))*70) + 100;
@@ -31,15 +34,15 @@ export function score(rank){
   if(rank => 1){ //rank has to be above 1
     if(rank < 11)//if rank is 10 or above
     { 
-      return round(top10Score);
+      return round(top10Score, scale);
     }
     else if(rank > 10 & rank < 51)//if rank is between 11 and 50
     { 
-      return round(top11_50Score)
+      return round(top11_50Score, scale)
     }
     else if(rank > 50 && rank < 76)//if rank is between 51 and 75
     { 
-      return round(top51_75Score);
+      return round(top51_75Score, scale);
     }
     else //return 0 if rank goes beyond 75 (it counts as legacy)
     {
@@ -47,9 +50,6 @@ export function score(rank){
     }
   }
 }
-
-const scale = 2; //Select how many decimal digits to round to
-
 /** Returns a rounded number by decimal point, set by scale
  * @param {Number} num      Number to round
  * @param {Number} scale    Number of decimal points to round to

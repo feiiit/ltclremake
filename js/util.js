c03e32359db09845050497c1ae1239e678f4af7c
slickomode <geometrydashslick@gmail.com>
2024-07-17 18:26:04 +0300

    getfontcolour expanded

--- js/util.js
+++ js/util.js
@@ -36,15 +36,33 @@ export function shuffle(array) {
 
     return array;
 }
-
-export function getFontColour(hexColor){
-    function getsRGB(c) {
+export function getFontColour(hex){
+    function getRGB(c) {
         return parseInt(c, 16) || c;
     }
+      
+      function getsRGB(c) {
+        return getRGB(c) / 255 <= 0.03928
+          ? getRGB(c) / 255 / 12.92
+          : Math.pow((getRGB(c) / 255 + 0.055) / 1.055, 2.4);
+    }
+      
+      function getLuminance(hexColor) {
+        return (
+          0.2126 * getsRGB(hexColor.substr(1, 2)) +
+          0.7152 * getsRGB(hexColor.substr(3, 2)) +
+          0.0722 * getsRGB(hexColor.substr(-2))
+        );
+    }
+      
+      function getContrast(f, b) {
+        const L1 = getLuminance(f);
+        const L2 = getLuminance(b);
+        return (Math.max(L1, L2) + 0.05) / (Math.min(L1, L2) + 0.05);
+    }
 
-    const brightness = Math.round(((getsRGB(hexColor.substr(1, 2)))*299 +
-                      (getsRGB(hexColor.substr(3, 2))*587) +
-                      (getsRGB(hexColor.substr(-2))*114))/1000);
+    const whiteContrast = getContrast(hex, "#ffffff");
+    const blackContrast = getContrast(hex, "#000000");
     
-    return (brightness > 125) ? 'black' : 'white';
+    return whiteContrast > blackContrast ? "#ffffff" : "#000000";
 }

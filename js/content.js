767dad42c6aef2e6f54b615c737ad7302a738a8f
Kandy <57315055+KandyMun@users.noreply.github.com>
2024-07-18 04:22:29 +0300

    Oh I really did it now

--- js/content.js
+++ js/content.js
@@ -101,15 +101,6 @@ export async function fetchLeaderboard() {
                 });
                 return;
             }
-
-            progressOnLevels.push({
-                rank: rank + 1,
-                level: level.name,
-                percent: record.percent,
-                score: score(rank + 1, record.percent, level.percentToQualify),
-                link: record.link,
-                path: level.path
-            });
         });
     });
 

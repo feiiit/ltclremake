a86d409fecb032f8cbfec29873c58c2fcebe89bc
Kandy <57315055+KandyMun@users.noreply.github.com>
2024-07-18 17:31:45 +0300

    Update to button

--- js/pages/List.js
+++ js/pages/List.js
@@ -58,7 +58,7 @@ export default {
                         </li>
                     </ul>
                     <div class="NONG-button" v-if="level.NONG != undefined"
-                        <Btn class="NONGdownload" onclick="location.href = level.NONG;">Atsisiųsti dainą</Btn>
+                        <button class="NONGdownload" onclick="location.href = level.NONG;">Atsisiųsti dainą</button>
                     </div>
                     <h2>Rekordai</h2>
                     <table class="records">

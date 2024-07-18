dd86b8539d888521f3186edd903ab433ba462357
Kandy <57315055+KandyMun@users.noreply.github.com>
2024-07-18 03:36:21 +0300

    reverting to current code, a completely different error occured

--- js/content.js
+++ js/content.js
@@ -1,192 +1,3 @@
-// import { round, score } from './score.js';
-
-// /**
-//  * Path to directory containing `_list.json` and all levels
-//  */
-// const dir = '/data';
-
-// export async function fetchList() {
-//     const listResult = await fetch(`${dir}/_list.json`);
-//     const packResult = await fetch(`${dir}/_packlist.json`);
-//     try {
-//         const list = await listResult.json();
-//         const packsList = await packResult.json();
-//         return await Promise.all(
-//             list.map(async (path, rank) => {
-//                 const levelResult = await fetch(`${dir}/${path}.json`);
-//                 try {
-//                     const level = await levelResult.json();
-//                     let packs = packsList.filter((x) =>
-//                         x.levels.includes(path)
-//                     );
-//                     return [
-//                         {
-//                             ...level,
-//                             packs,
-//                             path,
-//                             records: level.records,
-//                         },
-//                         null,
-//                     ];
-//                 } catch {
-//                     console.error(`Nepavyko užkrauti lygio: #${rank + 1} ${path}.`);
-//                     return [null, path];
-//                 }
-//             }),
-//         );
-//     } catch {
-//         console.error(`Nepavyko užkrauti sąrašo.`);
-//         return null;
-//     }
-// }
-
-
-// export async function fetchEditors() {
-//     try {
-//         const editorsResults = await fetch(`${dir}/_editors.json`);
-//         const editors = await editorsResults.json();
-//         return editors;
-//     } catch {
-//         return null;
-//     }
-// }
-
-// export async function fetchLeaderboard() {
-//     const list = await fetchList();
-//     const packResult = await (await fetch(`${dir}/_packlist.json`)).json();
-//     const player = {};
-//     const errs = [];
-//     list.forEach(([level, err], rank) => {
-//         if (err) {
-//             errs.push(err);
-//             return;
-//         }
-
-//         // Verification
-//         const verifier = Object.keys(player).find(
-//             (u) => u.toLowerCase() === level.verifier.toLowerCase(),
-//         ) || level.verifier;
-//         player[verifier] ??= {
-//             verifiedLevels: [],
-//             completedLevels: [],
-//             progressOnLevels: [],
-//             packsComplete: [],
-//         };
-//         const { verifiedLevels } = player[verifier];
-//         verifiedLevels.push({
-//             rank: rank + 1,
-//             level: level.name,
-//             score: score(rank + 1, 100, level.percentToQualify),
-//             link: level.verification,
-//         });
-
-//         // Records
-//         level.records.forEach((record) => {
-//             const user = Object.keys(player).find(
-//                 (u) => u.toLowerCase() === record.user.toLowerCase(),
-//             ) || record.user;
-//             player[user] ??= {
-//                 verifiedLevels: [],
-//                 completedLevels: [],
-//                 progressOnLevels: [],
-//                 packsComplete: [],
-//             };
-//             const { completedLevels } = player[user];
-//             if (record.percent === 100) {
-//                 completedLevels.push({
-//                     rank: rank + 1,
-//                     level: level.name,
-//                     score: score(rank + 1, 100, level.percentToQualify),
-//                     link: record.link,
-//                 });
-//                 return;
-//             }
-
-//             progressOnLevels.push({
-//                 rank: rank + 1,
-//                 level: level.name,
-//                 percent: record.percent,
-//                 score: score(rank + 1, record.percent, level.percentToQualify),
-//                 link: record.link,
-//             });
-//         });
-//     });
-
-//     //Player completed packs
-//     for (let user of Object.entries(player)) {
-//         let completions = [...user[1]["verifiedLevels"], ...user[1]["completedLevels"]].map(
-//             (x) => x["levels"]
-//         );
-    
-//         for (let pack of packResult) {
-//             if (pack.levels.every((packLevel) => completions.includes(packLevel))) {
-//                 user[1]["packsComplete"].push(pack);
-//             }
-//         }
-//     }
-
-//     // Wrap in extra Object containing the user and total score
-//     const res = Object.entries(player).map(([user, scores]) => {
-//         const { verifiedLevels, completedLevels, progressOnLevels } = scores;
-//         const total = [verifiedLevels, completedLevels, progressOnLevels]
-//             .flat()
-//             .reduce((prev, cur) => prev + cur.score, 0);
-
-//         return {
-//             user,
-//             total: round(total),
-//             ...scores,
-//         };
-//     });
-//     // Sort by total score
-//     return [res.sort((a, b) => b.total - a.total), errs];
-
-// }
-
-
-
-
-// export async function fetchPacks() {
-//     try {
-//         const packResult = await fetch(`${dir}/_packlist.json`);
-//         const packsList = await packResult.json();
-//         return packsList;
-//     } catch {
-//         return null;
-//     }
-// }
-
-// export async function fetchPackLevels(packname) {
-//     const packResult = await fetch(`${dir}/_packlist.json`);
-//     const packsList = await packResult.json();
-//     const selectedPack = await packsList.find((pack) => pack.name == packname);
-//     try {
-//         return await Promise.all(
-//             selectedPack.levels.map(async (path, rank) => {
-//                 const levelResult = await fetch(`${dir}/${path}.json`);
-//                 try {
-//                     const level = await levelResult.json();
-//                     return [
-//                         {
-//                             level,
-//                             path,
-//                             records: level.records.sort(
-//                                 (a, b) => b.percent - a.percent,
-//                             ),
-//                         },
-//                         null,
-//                     ];
-//                 } catch {
-//                     console.error(`Nepavyko užkrauti lygio: #${rank + 1} ${path} (${packname}).`);
-//                     return [null, path];
-//                 }
-//             })
-//         );
-//     } catch (e) {
-//         console.error(`Nepavyko užkrauti pakelių.`, e);
-//         return null;
-//     }
-// }
 import { round, score } from './score.js';
 
 /**
@@ -218,16 +29,17 @@ export async function fetchList() {
                         null,
                     ];
                 } catch {
-                    console.error(`Failed to load level #${rank + 1} ${path}.`);
+                    console.error(`Nepavyko užkrauti lygio: #${rank + 1} ${path}.`);
                     return [null, path];
                 }
             }),
         );
     } catch {
-        console.error(`Failed to load list.`);
+        console.error(`Nepavyko užkrauti sąrašo.`);
         return null;
     }
 }
+
 
 export async function fetchEditors() {
     try {
@@ -242,7 +54,7 @@ export async function fetchEditors() {
 export async function fetchLeaderboard() {
     const list = await fetchList();
     const packResult = await (await fetch(`${dir}/_packlist.json`)).json();
-    const scoreMap = {};
+    const player = {};
     const errs = [];
     list.forEach(([level, err], rank) => {
         if (err) {
@@ -251,62 +63,72 @@ export async function fetchLeaderboard() {
         }
 
         // Verification
-        const verifier = Object.keys(scoreMap).find(
+        const verifier = Object.keys(player).find(
             (u) => u.toLowerCase() === level.verifier.toLowerCase(),
         ) || level.verifier;
-        scoreMap[verifier] ??= {
-            verified: [],
-            completed: [],
-            progressed: [],
-            packs: [],
+        player[verifier] ??= {
+            verifiedLevels: [],
+            completedLevels: [],
+            progressOnLevels: [],
+            packsComplete: [],
         };
-        const { verified } = scoreMap[verifier];
-        verified.push({
+        const { verifiedLevels } = player[verifier];
+        verifiedLevels.push({
             rank: rank + 1,
             level: level.name,
-            score: score(level.difficulty),
+            score: score(rank + 1, 100, level.percentToQualify),
             link: level.verification,
-            path: level.path
         });
 
         // Records
         level.records.forEach((record) => {
-            const user = Object.keys(scoreMap).find(
+            const user = Object.keys(player).find(
                 (u) => u.toLowerCase() === record.user.toLowerCase(),
             ) || record.user;
-            scoreMap[user] ??= {
-                verified: [],
-                completed: [],
-                packs: [],
-                path: level.path
+            player[user] ??= {
+                verifiedLevels: [],
+                completedLevels: [],
+                progressOnLevels: [],
+                packsComplete: [],
             };
-            const { completed } = scoreMap[user];
-            completed.push({
+            const { completedLevels } = player[user];
+            if (record.percent === 100) {
+                completedLevels.push({
+                    rank: rank + 1,
+                    level: level.name,
+                    score: score(rank + 1, 100, level.percentToQualify),
+                    link: record.link,
+                });
+                return;
+            }
+
+            progressOnLevels.push({
                 rank: rank + 1,
                 level: level.name,
-                score: score(level.difficulty),
+                percent: record.percent,
+                score: score(rank + 1, record.percent, level.percentToQualify),
                 link: record.link,
-                path: level.path
             });
         });
     });
 
-    for (let user of Object.entries(scoreMap)) {
-        let levels = [...user[1]["verified"], ...user[1]["completed"]].map(
-            (x) => x["path"]
+    //Player completed packs
+    for (let user of Object.entries(player)) {
+        let completions = [...user[1]["verifiedLevels"], ...user[1]["completedLevels"]].map(
+            (x) => x["levels"]
         );
-
+    
         for (let pack of packResult) {
-            if (pack.levels.every((e1) => levels.includes(e1))) {
-                user[1]["packs"].push(pack);
+            if (pack.levels.every((packLevel) => completions.includes(packLevel))) {
+                user[1]["packsComplete"].push(pack);
             }
         }
     }
 
     // Wrap in extra Object containing the user and total score
-    const res = Object.entries(scoreMap).map(([user, scores]) => {
-        const {verified, completed} = scores;
-        const total = [verified, completed]
+    const res = Object.entries(player).map(([user, scores]) => {
+        const { verifiedLevels, completedLevels, progressOnLevels } = scores;
+        const total = [verifiedLevels, completedLevels, progressOnLevels]
             .flat()
             .reduce((prev, cur) => prev + cur.score, 0);
 
@@ -316,10 +138,13 @@ export async function fetchLeaderboard() {
             ...scores,
         };
     });
-
     // Sort by total score
     return [res.sort((a, b) => b.total - a.total), errs];
+
 }
+
+
+
 
 export async function fetchPacks() {
     try {
@@ -352,13 +177,13 @@ export async function fetchPackLevels(packname) {
                         null,
                     ];
                 } catch {
-                    console.error(`Failed to load level #${rank + 1} ${path} (${packname}).`);
+                    console.error(`Nepavyko užkrauti lygio: #${rank + 1} ${path} (${packname}).`);
                     return [null, path];
                 }
             })
         );
     } catch (e) {
-        console.error(`Failed to load packs.`, e);
+        console.error(`Nepavyko užkrauti pakelių.`, e);
         return null;
     }
 }

d9c9c5d0869b780bfa2e634b8428769f48d8c723
feiiit <lagzinsta@gmail.com>
2024-09-01 17:11:01 +0300

    gal

---
+++ js/pages/ModPanel.js
@@ -0,0 +1,16 @@
+import { fetchList } from '../content.js';
+import { getThumbnailFromId, getYoutubeIdFromUrl, shuffle } from '../util.js';
+
+import Spinner from '../components/Spinner.js';
+import Btn from '../components/Btn.js';
+
+export default {
+    components: { Spinner, Btn },
+    template: `
+        <main v-if="loading">
+            <Spinner></Spinner>
+        </main>
+        <main v-else class="page-roulette">
+            hi
+        </main>`
+};

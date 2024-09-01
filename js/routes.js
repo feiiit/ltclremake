8cf2d6842d44babe4a91069d65480039266da3f7
feiiit <lagzinsta@gmail.com>
2024-09-01 17:10:06 +0300

    PRIDEJAU ADMIN PAGE YIPEEE

--- js/routes.js
+++ js/routes.js
@@ -2,10 +2,12 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import ListPacks from './pages/ListPacks.js'
+import ModPanel from "./pages/ModPanel.js"
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
-    { path: '/list-packs', component: ListPacks },
+    {path: '/list-packs', component: ListPacks },
+    {path: '/admin', component: ModPanel },
 ];
